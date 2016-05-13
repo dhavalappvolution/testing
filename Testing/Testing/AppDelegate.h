40115f42f4dcd419c6ddcc01500dd7a74e72ae0c
Harry <mobile.uiresponder@gmail.com>
2016-05-13 06:39:12 -0400

    First Commit

---
+++ Testing/Testing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Testing
+//
+//  Created by Harry on 5/13/16.
+//  Copyright © 2016 Harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
